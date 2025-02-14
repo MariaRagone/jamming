08a7b3e2cf206d3cf31969e8afe6092f6df5243b
mragone <maria.ragone@samaritanministries.org>
2025-02-14 09:33:31 -0500

    made the header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+function Header() {
+  return (
+    <div className="header">
+      ja<span className="highlight">mmm</span>ing
+    </div>
+  );
+}
+export default Header;
