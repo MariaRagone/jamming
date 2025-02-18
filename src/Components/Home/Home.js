820b1ad51403c1147b883e1bfa6164474fa26bf3
mragone <maria.ragone@samaritanministries.org>
2025-02-18 14:58:05 -0500

    add and remove track functions

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import SearchResults from "../SearchResults/SearchResults";
 import ResultsContainer from "../ResultsContainer/ResultsContainer";
 
 const Home = () => {
+  const [playListTracks, setPlayListTrack] = useState([
+    {
+      name: "name",
+      artist: "artist",
+      album: "album",
+    },
+  ]);
+  const addTrack = (track) => {
+    setPlayListTrack((playList) => [track, ...[playListTracks]]);
+  };
+
+  const removeTrack = (trackNameToRemove) => {
+    setPlayListTrack((tracks) =>
+      playListTracks.filter((track) => track.name !== trackNameToRemove)
+    );
+  };
   return (
     <div>
       <ResultsContainer />
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,24 +1,16 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import "./Track.css";
 
-export const trackList = () => {
-  const [track, setTrack] = useState([]);
-};
-
-const addTrack = (track, setTrack, newTrack) => {
-  setTrack([...track, newTrack]);
-};
-const Track = ({ track, onAddTrack }) => {
+const Track = ({ track }) => {
   console.log(track);
   return (
     <div className="track-card">
       <div className="track-header">
         <h1>{track.name}</h1>
         <span className="plus-icon">
-          <FontAwesomeIcon icon={faPlus} onClick={() => onAddTrack(track)} />
+          <FontAwesomeIcon icon={faPlus} />
         </span>
       </div>
       <p>
